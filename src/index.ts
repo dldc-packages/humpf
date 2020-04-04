d41c37f0a88ee77319908f65fbef0c497437866d
Etienne Dldc <e.deladonchamps@gmail.com>
2020-04-05 00:13:29 +0200

    Export SpringFn type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { AngularFrequency } from './AngularFrequency';
 export { Spring } from './Spring';
 export { SpringBuilder } from './SpringBuilder';
-export { SpringConfig, SpringResult } from './types';
+export { SpringConfig, SpringResult, SpringFn } from './types';
