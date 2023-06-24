1f1b8fd9c763205f970996c0bab696d7758f52ce
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-24 14:03:46 +0200

    Update setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    threads: false, // needed for canvas https://github.com/vitest-dev/vitest/issues/740
+    threads: false,
   },
 });
