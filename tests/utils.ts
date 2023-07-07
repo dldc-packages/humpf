b6812ef472464143b3caf14bd07e90d8d8eda1ba
Etienne Deladonchamps <etienne@husprey.com>
2023-07-07 16:17:14 +0200

    Remove unused

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,35 +1,7 @@
-import { createCanvas, CanvasRenderingContext2D } from 'canvas';
-import { SpringFn } from '../src/mod';
+import { CanvasRenderingContext2D, createCanvas } from 'canvas';
 import { createWriteStream } from 'fs';
 import { resolve } from 'path';
-
-export type AsciiGraphConfig = {
-  width: number;
-  height: number;
-  xAxis: [min: number, max: number];
-  yAxis: [min: number, max: number];
-};
-
-export function asciiGraph(fn: (x: number) => number, { xAxis, yAxis, width, height }: AsciiGraphConfig): string {
-  const cols: Array<string> = [];
-  for (let x = 0; x < width; x++) {
-    const col = Array.from({ length: height }, () => '_');
-    const fnX = map(0, width, xAxis[0], xAxis[1], x);
-    const fnY = Math.round(map(yAxis[0], yAxis[1], 0, height, fn(fnX)));
-    if (fnY >= 0 && fnY < height) {
-      col[fnY] = '█';
-    }
-    cols.push(col.reverse().join(''));
-  }
-  let res = '';
-  for (let y = 0; y < height; y++) {
-    res += cols.map((col) => col[y]).join('');
-    if (y < height - 1) {
-      res += '\n';
-    }
-  }
-  return res;
-}
+import { SpringFn } from '../src/mod';
 
 export function map(inMin: number, inMax: number, outMin: number, outMax: number, num: number): number {
   return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
