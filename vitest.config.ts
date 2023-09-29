d5f31e9c97362c01dcaf75e61ff5b08ad9f5a324
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-09-29 22:37:12 +0200

    Upgrade setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [],
-  test: { threads: false },
+  test: {},
 });
