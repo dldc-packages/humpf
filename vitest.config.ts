c4377c38e88a993548cb3bf41233e19eba6a32dc
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-12-16 11:36:34 +0100

    Fix formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [],
-  test: {
-    poolOptions: { threads: { singleThread: true } },
-  },
+  test: { poolOptions: { threads: { singleThread: true } } },
 });
